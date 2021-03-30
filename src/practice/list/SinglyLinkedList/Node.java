22b50cc1c61e27e482375c9a88f70a96ddee736a
EunBin Lee <lwb001122@naver.com>
2021-03-30 17:57:06 +0900

    study SinglyLinkedList

---
+++ src/practice/list/SinglyLinkedList/Node.java
@@ -0,0 +1,12 @@
+package practice.list.SinglyLinkedList;
+
+class Node<E> {
+    E data;
+    Node<E> next; // 다음 노드 객체를 가리키는 레퍼런스 변수
+
+    Node(E data)
+    {
+        this.data = data;
+        this.next = null;
+    }
+}
